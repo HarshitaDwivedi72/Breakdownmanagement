26e710d1de11a95f03fba0cac74a865c69d79567
HarshitaDwivedi72 <51255281+HarshitaDwivedi72@users.noreply.github.com>
2022-08-17 17:04:56 +0530

    Updated Navigation

--- App.js
+++ App.js
@@ -1,29 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
+import 'react-native-gesture-handler';
+import { NativeBaseProvider, Box } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './src/Main';
+import CustomerDeliveries from './src/CustomerDeliveries';
+import CustomerPickups from './src/CustomerPickups';
+import SellerDeliveries  from './src/SellerDeliveries';
+import SellerPickups from './src/SellerPickups';
 
-import {
-  StyleSheet,
-  View,
-  Text
-} from 'react-native';
+const Stack = createStackNavigator();
 
-const App = () => {
+function App() {
   return (
-    <View>
-    <Text>////</Text>
-   </View>
-  );
-};
 
-const styles = StyleSheet.create({
- 
-});
+    <NativeBaseProvider>
+      <NavigationContainer>
+      <Stack.Navigator initialRouteName={'Login'}>
+
+          <Stack.Screen name="LoginScreen" component={LoginScreen}
+            options={{
+              header: () => null
+            }} /> 
+           <Stack.Screen name="Main" component={Main}
+            options={{
+              header: () => null
+            }} />
+          <Stack.Screen name="CustomerDeliveries" component={CustomerDeliveries}
+            options={{
+              header: () => null
+            }} />
+
+          <Stack.Screen name="CustomerPickups" component={CustomerPickups} 
+          options={{
+            header: () => null
+            }} />
+
+          <Stack.Screen name="SellerDeliveries" component={SellerDeliveries} 
+           options={{
+            header: () => null
+            }}/>
+
+          <Stack.Screen name="SellerPickups" component={SellerPickups} 
+           options={{
+            header: () => null
+           }} />
+
+        
+         
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
+
+
+
+  );
+}
+
 
 export default App;
+

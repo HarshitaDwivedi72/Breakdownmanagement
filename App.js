1fd47271f5502181c55a90ddf36eeec222ad469c
tanmay goyal <tanmaygoyal888@gmail.com>
2022-08-17 12:23:48 +0530

    setup

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+    <Text>////</Text>
+   </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
